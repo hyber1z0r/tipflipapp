588d40542523e8f432ee117286ed2aabc50950f8
Filipovic <Damii93@hotmail.com>
2015-04-26 14:23:59 +0200

    tried to make "Se hvor du er" google maps in location view

--- app/src/main/java/com/ohnana/tipflip/LocationFragment.java
+++ app/src/main/java/com/ohnana/tipflip/LocationFragment.java
@@ -5,11 +5,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
+
 public class LocationFragment extends CustomFragment implements View.OnClickListener {
+
 
     private MainActivity ma;
     private static LocationFragment instance;
     public static String TAG = "LOCATIONFRAGMENT";
+    private static View view;
+
 
     @Override
     protected boolean canGoBack() {
@@ -18,13 +23,28 @@ public class LocationFragment extends CustomFragment implements View.OnClickList
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.location_view, container, false);
+        if (container == null) {
+            return null;
+        }
+        view = inflater.inflate(R.layout.location_view, container, false);
         int[] clickButtons = new int[]{ // buttons
         };
         for (int i : clickButtons) {
             rootView.findViewById(i).setOnClickListener(this);
         }
         ma = (MainActivity) getActivity();
+
+       // Location location = ma.displayLocation();
+       // LatLng YOU = new LatLng(location.getLatitude(), location.getLongitude());
+       //   Marker youarehere = map.addMarker(new MarkerOptions().position(YOU)
+          //        .title("Du er her"));
+        // Move the camera instantly to hamburg with a zoom of 15.
+        //     map.moveCamera(CameraUpdateFactory.newLatLngZoom(YOU, 15));
+        // Zoom in, animating the camera.
+        //      map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+
+
+
         return rootView;
     }
 
@@ -38,7 +58,6 @@ public class LocationFragment extends CustomFragment implements View.OnClickList
         }
         return instance;
     }
-
 
 
     @Override
