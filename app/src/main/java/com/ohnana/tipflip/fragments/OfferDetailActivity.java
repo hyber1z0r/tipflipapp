f04726f546c8b4dccda6dba89e42841a4e5147f6
Messikrkic <Messikrkic@gmail.com>
2015-05-12 13:58:03 +0200

    detailview

--- app/src/main/java/com/ohnana/tipflip/fragments/OfferDetailActivity.java
+++ app/src/main/java/com/ohnana/tipflip/fragments/OfferDetailActivity.java
@@ -3,18 +3,34 @@ package com.ohnana.tipflip.fragments;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.ohnana.tipflip.R;
 import com.ohnana.tipflip.model.Offer;
+import com.ohnana.tipflip.model.Store;
 
 import org.parceler.Parcels;
 import org.parceler.transfuse.annotations.OnBackPressed;
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class OfferDetailActivity extends ActionBarActivity {
+
+    private TextView expview;
+    private TextView startview;
+    private TextView description;
+    private TextView storename;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +43,36 @@ public class OfferDetailActivity extends ActionBarActivity {
         if (extras != null) {
             Offer value = Parcels.unwrap(extras.getParcelable("Offer"));
             String title = value.getOffer();
+            Date start = value.getCreated();
+            Date exp = value.getExpiration();
+            String discount = value.getDiscount();
+            Store store = value.getStore();
+
+
+
+            SimpleDateFormat sdfDenmark = new SimpleDateFormat("dd-MM-yyyy hh:mm");
+            sdfDenmark.setTimeZone(TimeZone.getDefault());
+            String startfor = sdfDenmark.format(start);
+            String startexp = sdfDenmark.format(exp);
+
+            SpannableString ss =  new SpannableString("Der er tilbud på " + title + " Du kan sparer helt op til " + discount + "!");
+            ss.setSpan(new ForegroundColorSpan(Color.BLUE), (ss.length()-5), (ss.length()-1), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+
+
+
             getSupportActionBar().setTitle(title);
+            expview = (TextView) findViewById(R.id.textViewExp);
+            startview = (TextView) findViewById(R.id.textViewstart);
+            description = (TextView) findViewById(R.id.textViewDescription);
+            storename = (TextView) findViewById(R.id.textViewStore);
+
+            startview.setText("Oprettet: " + startfor);
+            expview.setText("Udløber: " + startexp);
+            storename.setText(store.getName());
+            description.setText(ss);
+
+
 
         }
     }
